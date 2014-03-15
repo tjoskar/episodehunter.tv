13eb60a3a4da5282f4a88e700d9a0e64c94945d8
Oskar Karlsson <mr.karlsson.oskar@gmail.com>
2014-03-15 20:36:49 +0100

    Added storage tests

---
+++ spec/lib/storage.tests.js
@@ -0,0 +1,44 @@
+/* global inject */
+describe('StorageTest', function(){
+    var _storage;
+
+    beforeEach(module('EHM'));
+
+    beforeEach(inject(function(storage) {
+        _storage = storage;
+        _storage.clearAll();
+    }));
+
+    it('should store a string', function() {
+        var key = 'key';
+        var val = 'value';
+        _storage.set(key, val);
+        var actual = _storage.get(key);
+
+        expect(actual).toEqual(val);
+    });
+
+    it('should store a object', function() {
+        var key = 'key';
+        var val = {
+            'key': 'val'
+        };
+        _storage.set(key, val);
+        var actual = _storage.get(key);
+
+        expect(actual).toEqual(val);
+    });
+
+    it('should be obsolete', function() {
+        var key = 'key';
+        var val = 'val';
+
+        _storage.set(key, val, 1000);
+        var storageData = _storage.get(key);
+        expect(_storage.isObsolete()).toBeFalsy();
+
+        _storage.set(key, val, -1);
+        storageData = _storage.get(key);
+        expect(_storage.isObsolete()).toBeTruthy();
+    });
+});
