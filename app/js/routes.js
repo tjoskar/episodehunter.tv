2a066cb86e489b11dab2c4b4478ba1188d7c949c
Oskar Karlsson <mr.karlsson.oskar@gmail.com>
2014-03-16 13:58:03 +0100

    Added some space

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,37 +1,37 @@
 angular.module("EHW").config(function($routeProvider) {
 
     $routeProvider.when('/home', {
-      templateUrl: 'upcoming.html',
-      controller: 'UpcomingController'
+        templateUrl: 'upcoming.html',
+        controller: 'UpcomingController'
     });
 
     $routeProvider.when('/tv/popular', {
-      templateUrl: 'popular.html',
-      controller: 'PopularTvController'
+        templateUrl: 'popular.html',
+        controller: 'PopularTvController'
     });
 
     $routeProvider.when('/movie/popular', {
-      templateUrl: 'popular.html',
-      controller: 'PopularMovieController'
+        templateUrl: 'popular.html',
+        controller: 'PopularMovieController'
     });
 
     $routeProvider.when('/tv/:id/:name?/:episode_id?', {
-      template: ' ',
-      controller: 'TvController'
+        template: ' ',
+        controller: 'TvController'
     });
 
     $routeProvider.when('/movie/:id/:name?', {
-      template: ' ',
-      controller: 'MovieController'
+        template: ' ',
+        controller: 'MovieController'
     });
 
     $routeProvider.when('/logout', {
-      template: ' ',
-      controller: 'LogoutController'
+        template: ' ',
+        controller: 'LogoutController'
     });
 
     $routeProvider.otherwise({
-      redirectTo: '/home'
+        redirectTo: '/home'
     });
 
 });
