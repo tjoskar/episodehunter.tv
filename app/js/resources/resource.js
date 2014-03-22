3495bb3fd1974698a38ac7343c3a7240dbc780e9
Oskar Karlsson <mr.karlsson.oskar@gmail.com>
2014-03-22 18:27:23 +0100

    broadcast on http start/stop

--- app/js/resources/resource.js
+++ app/js/resources/resource.js
@@ -1,13 +1,43 @@
-angular.module("EHW").factory('resource', function($http, $q) {
+angular.module("EHW").factory('resource', function($rootScope, $http, $q) {
+
+    var ongoingHTTPCounter = 0;
+
+    var httpStart = function() {
+        ongoingHTTPCounter++;
+
+        if (ongoingHTTPCounter === 1) {
+            EH.ajaxStart();
+        }
+
+        $rootScope.$broadcast('httpStart', ongoingHTTPCounter);
+    };
+
+    var httpEnd = function() {
+        ongoingHTTPCounter--;
+
+        if (ongoingHTTPCounter === 0) {
+            EH.ajaxStop();
+        }
+
+        $rootScope.$broadcast('httpEnd', ongoingHTTPCounter);
+    };
+
+    var errorHandler = function(data, status) {
+        if (status === 401) {
+            console.log('Logout user');
+        } else {
+            console.log('Show error message');
+        }
+    };
 
     return {
 
         get: function(url) {
             var deferred = $q.defer();
-            EH.ajaxStart();
+            httpStart();
 
             $http.get(url).success(function(data) {
-                EH.ajaxStop();
+                httpEnd();
                 if (data && angular.isDefined(data.value)) {
                     deferred.resolve(data.value);
                 } else {
@@ -15,23 +45,20 @@ angular.module("EHW").factory('resource', function($http, $q) {
                     deferred.reject();
                 }
             }).error(function(data, status) {
-                EH.ajaxStop();
-                if (status === 401) {
-                    console.log('Logout user');
-                } else {
-                    console.log('Show error message');
-                }
+                httpEnd();
+                errorHandler(data, status);
+                deferred.reject();
             });
 
             return deferred.promise;
         },
 
-        post: function(url, post_data) {
+        post: function(url, postData) {
             var deferred = $q.defer();
             EH.ajaxStart();
 
-            $http.post(url, post_data).success(function(data) {
-                EH.ajaxStop();
+            $http.post(url, postData).success(function(data) {
+                httpEnd();
                 if (data && angular.isDefined(data.value)) {
                     deferred.resolve(data.value);
                 } else {
@@ -39,12 +66,8 @@ angular.module("EHW").factory('resource', function($http, $q) {
                     deferred.reject();
                 }
             }).error(function(data, status) {
-                EH.ajaxStop();
-                if (status === 401) {
-                    console.log('Logout user');
-                } else {
-                    console.log('Show error message');
-                }
+                httpEnd();
+                errorHandler(data, status);
                 deferred.reject();
             });
 
