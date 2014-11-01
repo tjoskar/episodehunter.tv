c1d01efa20a3ab40df18d044d3075308f4a615c2
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-11-01 10:59:51 +0100

    Added some style for landing page

--- app/js/sections/authentication/AuthController.js
+++ app/js/sections/authentication/AuthController.js
@@ -2,6 +2,7 @@
  * Auth controller
  */
 angular.module("EHW").controller('AuthController', function() {
+    "use strict";
 
     console.log('AuthController');
 
--- app/js/sections/header/headerController.js
+++ app/js/sections/header/headerController.js
@@ -1,12 +1,8 @@
 /**
  * Header controller
  */
-angular.module("EHW").controller('HeaderController', function($scope, $mdSidenav) {
+angular.module("EHW").controller('HeaderController', function() {
 
     console.log('HeaderController');
 
-    $scope.toggleSidenav = function() {
-        console.log('hej hej');
-        $mdSidenav('left').toggle();
-    };
 });
