78b6fe29970cccd975957cd9743d1c31e430bded
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-02-14 12:11:43 +0100

    Add upcoming endpoint for api mock

--- server/server.js
+++ server/server.js
@@ -5,10 +5,59 @@ app.use(function(req, res, next) {
   next();
 });
 
+var upcoming = {
+    episodes: [
+        {
+            ids: {
+                id: 567,
+                show: 2
+            },
+            title: 'All men must die',
+            season: 3,
+            episode: 2,
+            airs: '2015-02-18T01:00:00.000Z',
+            thumbnail: 'something.jpg',
+            show: {
+                ids: {
+                    id: 2
+                },
+                title: 'Game of thrones',
+                year: 2005,
+                poster: 'poster.jpg',
+                fanart: 'fanart.jpg'
+            }
+        }, {
+            ids: {
+                id: 568,
+                show: 2
+            },
+            title: 'Yoo',
+            season: 3,
+            episode: 3,
+            airs: '2015-02-25T01:00:00.000Z',
+            thumbnail: 'something.jpg',
+            show: {
+                ids: {
+                    id: 2
+                },
+                title: 'Game of thrones',
+                year: 2005,
+                poster: 'poster.jpg',
+                fanart: 'fanart.jpg'
+            }
+        }
+    ]
+};
+
+
 app.get('/', function (req, res) {
   res.json([{
       'title': 'Katten'
   }]);
+});
+
+app.get('/user/upcoming', function(req, res) {
+    res.json(upcoming);
 });
 
 var server = app.listen(8080, function () {
