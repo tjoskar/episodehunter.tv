1e5d2b17c8ce27bdee51b6ed36f780b19df41c10
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-02-15 18:50:13 +0100

    Move image and font on develop build

--- gulp/styles.js
+++ gulp/styles.js
@@ -8,13 +8,23 @@ var filter = require('gulp-filter');
 
 var paths = gulp.paths;
 
+gulp.task('dev-images', function () {
+  return gulp.src(paths.src + '/assets/images/**/*')
+    .pipe(gulp.dest(paths.tmp + '/serve/assets/images/'));
+});
+
+gulp.task('dev-misc', function () {
+  return gulp.src(paths.src + '/**/*.ico')
+    .pipe(gulp.dest(paths.tmp + '/'));
+});
+
 gulp.task('dev-fonts', function () {
   return gulp.src(mainBowerFiles(), {base: paths.vendor + '/materialize/font/'})
     .pipe(filter('**/*.{eot,svg,ttf,woff}'))
     .pipe(gulp.dest(paths.tmp + '/serve/font/'));
 });
 
-gulp.task('styles', ['dev-fonts'], function() {
+gulp.task('styles', ['dev-fonts', 'dev-misc', 'dev-images'], function() {
     return sass(paths.src + '/app/index.scss', { sourcemap: true })
         .on('error', function (err) {
             console.error('Error!', err.message);
