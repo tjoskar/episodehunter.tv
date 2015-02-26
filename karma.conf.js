920caf5f42d03c394f1eb0c7d3e1788ada66db7e
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-02-26 23:33:48 +0100

    Add support for karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,23 @@
 'use strict';
 
 module.exports = function(config) {
-
-  config.set({
-    autoWatch : false,
-
-    frameworks: ['jasmine'],
-
-    browsers : ['PhantomJS'],
-
-    plugins : [
-        'karma-phantomjs-launcher',
-        'karma-jasmine'
-    ]
-  });
+    config.set({
+        browsers : ['Chrome'],
+        frameworks: ['browserify', 'jasmine'],
+        files: [
+            '.tmp/serve/vendor.js',
+            '.tmp/serve/app/boot.js',
+            'spec/**/*.js'
+        ],
+        logLevel: config.LOG_WARN,
+        reporters: ['dots'],
+        preprocessors: {
+              'spec/**/*.js': [ 'browserify' ]
+        },
+        browserify: {
+            debug: true,
+            extensions: ['.js'],
+            transform: ['6to5ify']
+        },
+    });
 };
