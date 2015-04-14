110356d2442030932b7d76aba9cc8de97a8fb4b1
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-04-14 21:45:02 +0200

    FEATUE: Added CROS to mock server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var upcoming = require('./upcoming');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   next();
 });
 
