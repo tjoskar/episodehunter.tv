050d8be2bff8c976318c28b482cf8acf654c5ae5
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-06-21 08:36:43 +0200

    Use browserify instead of gulp-browserify

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,19 +1,24 @@
 'use strict';
 
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var babelify = require('babelify');
 var paths = gulp.paths;
 
 gulp.task('browserify', ['lint'], function () {
-  return gulp.src(paths.src + '/app/boot.js', { read: false })
-    .pipe($.browserify({
-        debug: true,
-        transform: ['babelify']
-    }))
-    .on('error', function handleError(err) {
-      console.error(err.toString());
-      this.emit('end');
-    })
-    .pipe(gulp.dest(paths.tmp + '/serve/app'))
-    .pipe($.size());
+    return browserify(paths.src + '/app/boot.js', { debug: true })
+        .transform(babelify.configure({
+            optional: [
+                'es7.classProperties',
+                'es7.functionBind'
+            ]
+        }))
+        .bundle()
+        .pipe(source('boot.js'))
+        .on('error', function handleError(err) {
+            console.error(err.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest(paths.tmp + '/serve/app'));
 });
