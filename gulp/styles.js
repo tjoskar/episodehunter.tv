7aee32ee5b93e4f7b6ab852a2d6f126109fccc8c
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-02-06 13:18:35 +0100

    Switch to a node sass

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,16 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var sass = require('gulp-ruby-sass');
+// var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 var paths = gulp.paths;
 
 gulp.task('styles', function() {
-    return sass(paths.src + '/app/index.scss', { sourcemap: true })
-        .on('error', function (err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(sourcemaps.write())
+    return gulp.src(paths.src + '/app/index.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(paths.tmp + '/serve/app/'))
         .pipe(gulp.dest(paths.dist));
 });
