11beb558e0bf2dd1c3693dc80b618cb00a9a8efb
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-06-12 10:55:07 +0200

    :art: Add support for HMR

--- src/boot.ts
+++ src/boot.ts
@@ -14,3 +14,29 @@ bootstrap(AppComponent, [
     AuthService,
     LocalStorage
 ]);
+
+
+function main() {
+    return bootstrap(AppComponent, [
+        ROUTER_PROVIDERS,
+        HTTP_PROVIDERS,
+        HttpService,
+        AuthService,
+        LocalStorage
+    ])
+    .then(() => '🦄')
+    .catch(err => console.error(err));
+}
+
+if (ENV === 'development' && HMR === true) {
+    // activate hot module reload
+    if (document.readyState === 'complete') {
+        console.clear();
+        main();
+    } else {
+        document.addEventListener('DOMContentLoaded', main);
+    }
+    module.hot.accept();
+} else {
+    throw new Error('Not implemented - prod mode');
+}
